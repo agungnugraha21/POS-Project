f385c0ce3703ed75a861963e3debdb2b8ef99d55
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-11-17 14:53:21 +0700

    nambah database

---
+++ POSDB.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.1.53-community-log : Database - pos
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `pos`;
+
+/*Table structure for table `produk` */
+
+DROP TABLE IF EXISTS `produk`;
+
+CREATE TABLE `produk` (
+  `kode_produk` varchar(10) NOT NULL,
+  `nama_produk` varchar(30) NOT NULL,
+  `harga` varchar(10) NOT NULL,
+  `id_kategori` int(5) NOT NULL,
+  PRIMARY KEY (`kode_produk`),
+  KEY `id_kategori` (`id_kategori`),
+  CONSTRAINT `produk_ibfk_2` FOREIGN KEY (`kode_produk`) REFERENCES `pemesanan` (`kode_produk`),
+  CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `produk` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
